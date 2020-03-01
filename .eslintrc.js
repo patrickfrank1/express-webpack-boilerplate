ed121caea326609f6453969977ffb1b57cd64d8e
Patrick Frank <patr.frank@gmx.de>
2020-03-01 14:11:22 +0100

    Fix initialise eslint and change config file to js
    Settings for initialisation:
    ? How would you like to use ESLint? To check syntax, find problems, and enforce code style
    ? What type of modules does your project use? CommonJS (require/exports)
    ? Which framework does your project use? None of these
    ? Does your project use TypeScript? No
    ? Where does your code run? Browser
    ? How would you like to define a style for your project? Use a popular style guide
    ? Which style guide do you want to follow? Airbnb: https://github.com/airbnb/javascript
    ? What format do you want your config file to be in? JavaScript

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "arrow-body-style": ["error", "always"],
+    "no-console": "warn",
+    "import/extensions": ["error", "always"]
+  },
+};
