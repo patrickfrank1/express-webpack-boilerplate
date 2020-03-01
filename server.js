299f4bf45eca9b825e65f4fc470f556d99b382cc
Patrick Frank <patr.frank@gmx.de>
2020-03-01 14:27:15 +0100

    Fix make app listen again in server.js

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use('/docs', express.static(path.join(__dirname, 'documentation')));
 app.set('port', process.env.PORT || 8080);
 
-/*
-const server = app.listen(app.get('port'), () => {
-  console.log('listening on port ', server.address().port);
-});
-*/
+
+// eslint-disable-next-line no-unused-vars
+const server = app.listen(app.get('port'), () => {});
